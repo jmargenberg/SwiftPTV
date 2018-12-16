158b2e03d5701d5baa44c437b697c76d395757cc
James Margenberg <james@margenberg.com.au>
2018-12-16 19:38:49 +1100

    URLSession mock classes

--- SwiftPTVTests/SwiftPTVTests.swift
+++ SwiftPTVTests/SwiftPTVTests.swift
@@ -1,34 +1,18 @@
-//
-//  SwiftPTVTests.swift
-//  SwiftPTVTests
-//
-//  Created by James Margenberg on 29/11/18.
-//  Copyright © 2018 James Margenberg. All rights reserved.
-//
-
 import XCTest
 @testable import SwiftPTV
 
 class SwiftPTVTests: XCTestCase {
+    private var urlSessionMock: URLSessionMock!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        urlSessionMock = URLSessionMock()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        urlSessionMock = nil
+        
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
