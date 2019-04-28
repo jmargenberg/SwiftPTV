3d83971fb011ec113c2ad7536aa27cde944349ec
James Margenberg <34509336+jmargenberg@users.noreply.github.com>
2019-04-28 14:49:22 +1000

    Made all properties under stop details optional. (#6)

--- PTV/Models/StopAmenityDetails.swift
+++ PTV/Models/StopAmenityDetails.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct StopAmenityDetails: Codable {
-    public let toilet: Bool
-    public let taxi_rank: Bool
-    public let cctv: Bool
+    public let toilet: Bool?
+    public let taxi_rank: Bool?
+    public let cctv: Bool?
 }
--- PTV/Models/StopDetails.swift
+++ PTV/Models/StopDetails.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct StopDetails: Codable {
     public let station_description: String
-    public let stop_amenities: StopAmenityDetails
-    public let stop_accessibility: StopAccessibility
+    public let stop_amenities: StopAmenityDetails?
+    public let stop_accessibility: StopAccessibility?
     public let stop_location: StopLocation?
 }
