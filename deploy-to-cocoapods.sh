e66dde6cba4c97bba7ee06aa0b6d1ac6ff781923
James Margenberg <james@margenberg.com.au>
2019-11-06 21:25:24 +1100

    Added fastlane execution to deploy script

--- deploy-to-cocoapods.sh
+++ deploy-to-cocoapods.sh
@@ -7,3 +7,6 @@ chmod 400 ./travis_key
 echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
 
 git remote set-url origin git@github.com:jmargenberg/SwiftPTV.git
+
+# Run cocoapods deployment with Fastlane
+bundle exec fastlane release
